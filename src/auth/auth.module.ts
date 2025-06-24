bfa5139061e9ca4feb6a8f0957ad565faa13f6c9
LAGZYA <60875085+lagzya-dev@users.noreply.github.com>
2025-06-24 12:13:30 +0300

    fixes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '7d' },
     }),
   ],
   controllers: [AuthController],
